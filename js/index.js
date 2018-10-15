0bfcb5f29b3b41aa61831befa6c115597025de3c
Abel Sobmenso Dadbutiso <abelsobmenso@gmail.com>
2018-10-15 22:02:00 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -6,3 +6,10 @@ const main = document.getElementById('main');
 main.addEventListener('click', function(event) {
   alert('I was clicked!');
 });
+
+```js```
+const input = document.querySelector('input');
+
+input.addEventListener('keydown', function(e) {
+  console.log(e.which);
+});
